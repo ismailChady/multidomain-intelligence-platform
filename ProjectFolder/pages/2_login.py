c1377a7beaeacdfa30714689c2d8c7763112f868
ismail Chady <MC2231@live.mdx.ac.uk>
2025-11-20 22:48:02 +0400

    Deleted ProjectFolder/pages/2_login.py

--- ProjectFolder/pages/2_login.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from Login import login_user
-
-st.title("Login")
-
-# Display login form using your existing login_user() function
-if login_user():
-    st.session_state.logged_in = True
-    st.success(f"Welcome back, {st.session_state.username}!")
