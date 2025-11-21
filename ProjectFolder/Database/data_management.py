5e55695b67e91cb479f3bd531607080579329a9a
ismailChady <MC2231@live.mdx.ac.uk>
2025-11-21 08:44:07 +0400

    updated code for data management

--- ProjectFolder/Database/data_management.py
+++ ProjectFolder/Database/data_management.py
@@ -2,15 +2,24 @@ import sqlite3
 
 DB_PATH = "Data/intelligence_platform.db"
 
-# Connect to DB
+#DB Connection
 def connect_db():
     return sqlite3.connect(DB_PATH)
 
-# USERS 
+#USERS 
+
 def get_all_users(conn):
     return conn.execute("SELECT id, username, role FROM users").fetchall()
 
-# CYBER INCIDENTS 
+def insert_user(conn, username, password_hash, role="user"):
+    conn.execute(
+        "INSERT INTO users (username, password_hash, role) VALUES (?, ?, ?)",
+        (username, password_hash, role)
+    )
+    conn.commit()
+
+#CYBER INCIDENTS 
+
 def get_all_incidents(conn):
     return conn.execute("SELECT * FROM cyber_incidents").fetchall()
 
@@ -21,7 +30,8 @@ def insert_incident(conn, title, severity, status, date):
     )
     conn.commit()
 
-#  IT TICKETS 
+#IT TICKETS
+
 def get_all_tickets(conn):
     return conn.execute("SELECT * FROM it_tickets").fetchall()
 
@@ -32,7 +42,8 @@ def insert_ticket(conn, title, priority, status, created_date):
     )
     conn.commit()
 
-# DATASET METADATA 
+#DATASETS
+
 def get_all_datasets(conn):
     return conn.execute("SELECT * FROM datasets_metadata").fetchall()
 
