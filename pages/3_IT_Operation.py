be2254ec111205aeeacda8415327e353094b7b85
ismailChady <MC2231@live.mdx.ac.uk>
2025-11-27 12:17:07 +0400

    adding crud operations

--- pages/3_IT_Operation.py
+++ pages/3_IT_Operation.py
@@ -51,3 +51,57 @@ if not filtered_df.empty:
 # Table
 st.subheader("Ticket Records")
 st.dataframe(filtered_df, use_container_width=True)
+
+st.divider()
+
+#Insert form
+st.subheader("Add New Ticket")
+with st.form("insert_ticket"):
+    priority = st.selectbox("Priority", ["Low", "Medium", "High", "Urgent"])
+    description = st.text_area("Description")
+    status = st.selectbox("Status", ["Open", "Assigned", "Resolved"])
+    assigned_to = st.text_input("Assigned To")
+    created_at = st.date_input("Created Date")
+    resolution_time = st.number_input("Resolution Time (hrs)", min_value=0.0)
+    submit = st.form_submit_button("Submit Ticket")
+
+    if submit:
+        conn = connect_db()
+        insert_query = """
+            INSERT INTO it_tickets (priority, description, status, assigned_to, created_at, resolution_time_hours)
+            VALUES (?, ?, ?, ?, ?, ?)
+        """
+        conn.execute(insert_query, (priority, description, status, assigned_to, str(created_at), resolution_time))
+        conn.commit()
+        conn.close()
+        st.success("Ticket submitted successfully.")
+        st.experimental_rerun()
+
+#Upate form
+st.subheader("Update Ticket Status")
+with st.form("update_ticket"):
+    selected_id = st.selectbox("Select Ticket ID", df["Ticket ID"])
+    new_status = st.selectbox("New Status", ["Open", "Assigned", "Resolved"])
+    submit_update = st.form_submit_button("Update Status")
+
+    if submit_update:
+        conn = connect_db()
+        conn.execute("UPDATE it_tickets SET status = ? WHERE ticket_id = ?", (new_status, selected_id))
+        conn.commit()
+        conn.close()
+        st.success(f"Ticket {selected_id} status updated to '{new_status}'.")
+        st.experimental_rerun()
+
+#Delete form
+st.subheader("Delete Ticket")
+with st.form("delete_ticket"):
+    del_id = st.selectbox("Select Ticket to Delete", df["Ticket ID"], key="del_id")
+    submit_del = st.form_submit_button("Delete Ticket")
+
+    if submit_del:
+        conn = connect_db()
+        conn.execute("DELETE FROM it_tickets WHERE ticket_id = ?", (del_id,))
+        conn.commit()
+        conn.close()
+        st.warning(f"Ticket {del_id} deleted.")
+        st.experimental_rerun()
